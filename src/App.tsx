d05713a30ab93aee788ad86b39dac1d41b54ae48
abr-ya <gamer05@list.ru>
2022-11-13 14:19:37 +0300

    feature: Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, About, Error404 } from "./pages";
 import Navbar from "./components/layout/Navbar";
 import "./app.css";
+import Footer from "./components/layout/Footer";
 
 const App = () => (
   <Router>
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col justify-between h-screen">
       <Navbar title="gitApp" />
-      <main>
+      <main className="container mx-auto px-3 pb-12">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   </Router>
 );
