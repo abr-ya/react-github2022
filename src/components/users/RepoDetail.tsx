bdcf71d747aa2878863a9c182e308307621d387d
abr-ya <gamer05@list.ru>
2023-01-05 11:03:42 +0300

    feature: Update RepoDetail component

--- src/components/users/RepoDetail.tsx
+++ src/components/users/RepoDetail.tsx
@@ -1,7 +1,37 @@
+import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from "react-icons/fa";
 import { IRepo } from "../../interfaces";
 
-const RepoDetail = ({ repo }: { repo: IRepo }) => {
-  return <div>{repo.name}</div>;
+const RepoDetail = ({
+  repo: { name, description, html_url, forks, open_issues, watchers_count, stargazers_count },
+}: {
+  repo: IRepo;
+}) => {
+  return (
+    <div className="mb-2 rounded-md card bg-gray-300 hover:bg-gray-500">
+      <div className="card-body">
+        <h3 className="mb-2 text-xl font-semibold">
+          <a href={html_url} target="_blank" rel="noreferrer">
+            <FaLink className="inline mr-1" /> {name}
+          </a>
+        </h3>
+        <p className="mb-3">{description}</p>
+        <div>
+          <div className="mr-2 badge badge-info badge-lg">
+            <FaEye className="mr-2" /> {watchers_count}
+          </div>
+          <div className="mr-2 badge badge-success badge-lg">
+            <FaStar className="mr-2" /> {stargazers_count}
+          </div>
+          <div className="mr-2 badge badge-error badge-lg">
+            <FaInfo className="mr-2" /> {open_issues}
+          </div>
+          <div className="mr-2 badge badge-warning badge-lg">
+            <FaUtensils className="mr-2" /> {forks}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RepoDetail;
